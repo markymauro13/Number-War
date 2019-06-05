87648d9a42d739c0f436620aa67c20b7b7013f85
Mark Mauro <markymauro@gmail.com>
2019-06-05 13:46:05 -0400

    Create numberWar.java

---
+++ numberWar.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.*;  
+
+public class numberWar {
+
+	private JTextField player, computer;
+	private JButton go;
+	
+	public numberWar(){
+		JFrame frame = new JFrame("Number War");	// creates the JFrame and titles it
+	
+		player = new JTextField();
+		player.setBounds(25,100,125,20);
+		frame.add(player);
+		
+		// these must be after everything or else other components will not display
+		frame.setSize(400,400);	// sets the size of the frame
+        frame.setLayout(null);	// sets layout   
+		frame.setVisible(true);	// sets JFrame to be visible
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new numberWar();
+	}
+
+}
