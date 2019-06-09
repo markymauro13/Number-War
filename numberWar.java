f648e5eb11d9a385534697cd37cc6eb43c49fe12
Mark Mauro <markymauro@gmail.com>
2019-06-09 17:34:01 -0400

    Update numberWar.java

--- numberWar.java
+++ numberWar.java
@@ -1,29 +1,120 @@
+import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;  
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
-public class numberWar {
+public class numberWar implements ActionListener {
 
-	private JTextField player, computer;
+	private JTextField player, computer, winner, decoy;
 	private JButton go;
+	private JLabel playerLabel, computerLabel, numberWar;
+	private ImageIcon thumbsUp, thumbsDown;
+	private JLabel tU,tD;
+	private int range = 100;
+	private int randomNum = (int) (Math.random() * range) + 1;
 	
-	public numberWar(){
+	public numberWar() {
 		JFrame frame = new JFrame("Number War");	// creates the JFrame and titles it
-	
-		player = new JTextField();
-		player.setBounds(25,200,125,20);
-		frame.add(player);
 		
-		// these must be after everything or else other components will not display
-		frame.setSize(400,400);	// sets the size of the frame
+		Font font0 = new Font("Segoe UI",Font.BOLD,40);
+		numberWar = new JLabel("Number War!");
+		numberWar.setForeground(Color.BLACK);
+		playerLabel.setFont(font0);
+		playerLabel.setBounds(55,160,125,30);
+		frame.add(numberWar);
+		
+		Font font1 = new Font("Segoe UI",Font.BOLD,20);	
+		playerLabel = new JLabel("Player 1"); 
+		playerLabel.setForeground(Color.BLACK);
+		playerLabel.setFont(font1);
+		playerLabel.setBounds(55,160,125,30);
+		frame.add(playerLabel);
+		
+		
+		player = new JTextField();
+		player.setBounds(35,200,125,30);
+		frame.add(player);
+
+		
+		Font font2 = new Font("Segoe UI",Font.BOLD,20);
+		computerLabel = new JLabel("Computer"); 
+		computerLabel.setForeground(Color.BLACK);
+		computerLabel.setFont(font2);
+		computerLabel.setBounds(240,160,125,30);
+		frame.add(computerLabel);
+		
+		decoy = new JTextField("I already chose my #");
+		decoy.setBounds(220,200,125,30);
+		frame.add(decoy);
+		
+		computer = new JTextField("2");
+		computer.setBounds(220,200,125,30);
+		frame.add(computer);
+	
+		
+		go = new JButton("Go");
+		go.setBounds(164,250,50,30);
+		frame.add(go);
+	
+		
+		winner = new JTextField();
+		winner.setBounds(125,315,125,30);
+		frame.add(winner);
+		
+		computer.addActionListener(this);
+		player.addActionListener(this);
+		go.addActionListener(this);
+	    winner.setEditable(true);
+		
+	    frame.getContentPane().setBackground(Color.lightGray);
+		frame.setSize(400,450);	// sets the size of the frame
         frame.setLayout(null);	// sets layout   
 		frame.setVisible(true);	// sets JFrame to be visible
 	
 	}
 	
+	public int getRandomNumber() {
+		return randomNum;
+	}
+
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		String s1 = player.getText();
+        int a = Integer.parseInt(s1);  
+        
+        String s2 = computer.getText();
+        int b = Integer.parseInt(s2);
+        
+        if(e.getSource() == go) {
+        	if(a > range) {
+        		String result = "Pick a # <= 100";
+        		winner.setText(result);
+        	}
+        	else if(a > getRandomNumber()) {
+        		String result = "You win!";
+        		winner.setText(result);
+        	}
+        	else if(getRandomNumber() > a) {
+        		String result = "You lose!";
+        		winner.setText(result);
+        	}
+        	else {
+        		String result = "You tied!";
+        		winner.setText(result);
+        	}
+        }
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new numberWar();
 	}
+
+
 
 }
